af3b2507015539212a2e9273353f0223ed85fc43
William Breiler <will.breiler@gmail.com>
2024-12-09 15:21:55 -0600

    Updated list.php to fix Cloudflare caching issues

--- list.php
+++ list.php
@@ -1,8 +1,13 @@
 <?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 $directory = './cats';
 $images = array_values(array_filter(scandir($directory), function($file) {
     return preg_match('/\.(jpg|jpeg|png)$/i', $file);
 }));
+
 header('Content-Type: application/json');
 echo json_encode($images);
 ?>
