0811841bd339c38fcccab1f5fe624fbe773d3b29
William Breiler <will.breiler@gmail.com>
2024-12-09 15:30:32 -0600

    Updated regex in list.php

--- list.php
+++ list.php
@@ -4,10 +4,13 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
 $directory = './cats';
+
+// Scan the directory for valid image files
 $images = array_values(array_filter(scandir($directory), function($file) {
-    return preg_match('/\.(jpg|jpeg|png)$/i', $file);
+    return preg_match('/\.(jpe?g|png)$/i', $file); // Matches jpg, jpeg, JPG, JPEG, png, PNG
 }));
 
+// Set response type to JSON and return the list of images
 header('Content-Type: application/json');
 echo json_encode($images);
 ?>
